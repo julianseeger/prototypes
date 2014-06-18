def8bdf2963a6c60e7bec3d86d4eccdcd3a02d84
Julian <seeger.julian@gmail.com>
2014-06-18 03:02:11 +0200

    charts for numbers prototype

--- numbers/web/public/index.php
+++ numbers/web/public/index.php
@@ -23,19 +23,23 @@ $app->post('/{project}/{version}/{materialType}', function (Request $request, $p
         }
     }
 );
-$app->get('/loadmaterial', function (Request $request) {
-        $material = file_get_contents('clover.xml');
+$app->get('/loadmaterial', function () {
+        $material1 = file_get_contents('clover.xml');
+        $material2 = file_get_contents('clover2.xml');
+        $material3 = file_get_contents('clover3.xml');
 
         $interactor = new \Numbers\Interactor\AnalyzeMaterialInteractor();
         try {
-            $interactor->execute('Testproject', 'v0.1', 'clover', $material);
+            $interactor->execute('Testproject', 'v0.1', 'clover', $material1);
+            $interactor->execute('Testproject', 'v0.2', 'clover', $material2);
+            $interactor->execute('Testproject', 'v0.3', 'clover', $material3);
             return new Response("DONE", 201);
         } catch (\Exception $e) {
             return new Response($e->getMessage() . "<br/>" . $e->getTraceAsString(), 500);
         }
     }
 );
-$app->get('/', function (Request $request) use ($app) {
+$app->get('/', function () use ($app) {
         $interactor = new \Numbers\Interactor\SummaryInteractor();
         $summary = $interactor->execute();
 
